a85fd7898e3061926052d2dd284747d551182888
KrouTm <91268160+KrouTm@users.noreply.github.com>
2023-01-04 02:52:42 -0300

    make it better

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
